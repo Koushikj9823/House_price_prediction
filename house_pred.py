18b4750ea66873df41adfdfdc8f6706896ef9b04
Koushikj9823 <31768618+Koushikj9823@users.noreply.github.com>
2019-01-07 19:45:49 +0530

    Update house_pred.py

--- house_pred.py
+++ house_pred.py
@@ -17,64 +17,48 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 
-data=pd.read_csv('ex1data2.txt',header= None)
+data=pd.read_csv('ex1data2.txt',header= None) #read the dataset
 
-X = data.iloc[:,0:2] # read first two columns into X
-y = data.iloc[:,2] # read the third column into y
+X = data.iloc[:,0:2] # read the independent columns into X
+y = data.iloc[:,2] # read the dependent column into y
 m = len(y) # no. of training samples
-data.head()
-X = (X - np.mean(X))/np.std(X)
-from sklearn.cross_validation import train_test_split
+
+X = (X - np.mean(X))/np.std(X) #Feature scaling
+
+from sklearn.cross_validation import train_test_split #split into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-plt.scatter(X[:,1], y)
-plt.scatter(X[:,1:2], y)
-plt.ylabel('Price')
-plt.xlabel('House features')
-plt.show()
-#plt.scatter(X, y)
-#plt.xlabel('Population of City in 10,000s')
-#plt.ylabel('Profit in $10,000s')
-#plt.show()
-a=np.shape(y)
-#X = X[:,np.newaxis]
+
 y = y[:,np.newaxis]
 theta = np.zeros((3,1))
 alpha = 0.01
 ones = np.ones((m,1)) 
 X = np.hstack((ones, X))
+iterations = 1500
 
-
-def costfunc(X_train,y_train,theta):#
-        #temp = np.dot(X,theta)-y
-       # temp= (1/2*m)*pow(temp,2)
+def costfunc(X_train,y_train,theta): #Determine the Cost Function
        temp = np.dot(X_train, theta) - y_train
        return np.sum(np.power(temp, 2)) / (2*m)
 J = costfunc(X_train, y_train, theta)
 print(J)
         
-def grad(X,y,alpha,theta):
-    for i in range(1500):
+def grad(X,y,alpha,theta): #Gradient Descent to find global minima
+    for i in range(iterations):
         temp=np.dot(X,theta)-y
         temp=np.dot(X.T,temp)
         theta = theta - (alpha/m)*temp
     return theta
-theta = grad(X_train, y_train, alpha,theta)
+
+theta = grad(X_train, y_train, alpha,theta) #Calculate theta(Parameters)
 print(theta)   
         
 J = costfunc(X_train, y_train, theta)
-print(J)        
-f=theta[1]
-#p=[1,1600,3]
-#p = (p - np.mean(p))/np.std(p)
+print(J) 
+
+
 res=np.dot(X_test,theta)
 print(res)
-plt.scatter(X[:,1],y)
-plt.scatter(X[:,1:2], y)
-plt.xlabel('House_features')
-plt.ylabel('House_price')
-plt.plot(X[:,1:2], np.dot(X, theta))
-plt.show()
+
 
 
 
